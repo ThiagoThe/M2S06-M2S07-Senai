02b2c3d8e22af96758a330690907ac063ee5b353
ThiagoThe <nitrox2thi@gmail.com>
2023-06-22 15:04:14 -0300

    feat: ex2 e 3 criando tabelas e inserindo dados

--- SEMANA 07/Ex2_criando_tabelas.sql
+++ SEMANA 07/Ex2_criando_tabelas.sql
@@ -11,41 +11,44 @@ rg varchar(20) not null
 
 create table jogo (
 id serial primary key,
+usuario_id int,
+constraint fk_usuario foreign key (usuario_id)references usuario(id),
 nome varchar(100) not null,
-dt_lancamento date not null
+dt_lancamento date not null,
+plataforma_id int,
+constraint fk_plataforma foreign key (plataforma_id) references plataforma(id),
+genero varchar(50) not null,
+fotos_url varchar(200),
+videos_url varchar(200)
 );
 
-create table genero (
+create table plataforma(
 id serial primary key,
-nome varchar(50) not null
+tipo varchar(100),
+check (tipo in('PS1','PS2','PS3','PS4','PS5','XBOX SERIES S', 'XBOX SERIES X', 'PC'))
 );
 
-create table plataforma (
-id serial primary key,
-nome varchar(50) not null
-);
 
-create table jogo_genero (
-jogo_id int references jogo(id),
-genero_id int references genero(id),
-primary key(jogo_id, genero_id)
+-- Inserir o jogo "The Sims"
+insert into jogo (usuario_id, nome, dt_lancamento, plataforma_id, genero, fotos_url, videos_url)
+select 1, 'The Sims 4 Base', '2020-03-12', id,'Simulador', 'www.fotosgame.com', 'www.videosgame.com'
+from plataforma 
+where tipo in ('PS4', 'PS5','PC');
 
-);
 
-create table jogo_plataforma (
-jogo_id int references jogo(id),
-plataforma_id int references plataforma(id),
-primary key(jogo_id, plataforma_id)
-);
+insert into Usuario (nome, login, senha, email, dt_nasc, cpf, rg)
+values ('Derpson da Silva', 'derpinho', 'derpinho91', 'derpinho91@hotmail.com', '1991-01-01', '123.123.123-12', '4.123.123');
 
-create table foto (
-id serial primary key,
-jogo_id int references jogo(id),
-url varchar(200) not null
-);
+select * from plataforma ;
 
-create table video (
-id serial primary key,
-jogo_id int references jogo(id),
-url varchar(200) not null
-);
+select * from jogo;
+
+insert into plataforma (tipo)
+values ('PS1'),
+       ('PS2'),
+       ('PS3'),
+       ('PS4'),
+       ('PS5'),
+       ('XBOX SERIES S'),
+       ('XBOX SERIES X'),
+       ('PC');
